90c6cc2f5dc724725407b25995d36c1840ae7a1a
Jeffrey Chupp <jeff@semanticart.com>
2023-04-24 10:34:12 -0400

    Add faux users and context

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  include Authentication
+
   around_action do |_, block|
     $prefab.with_context(prefab_context, &block)
   end
@@ -7,6 +9,12 @@ class ApplicationController < ActionController::Base
     {
       device: {
         mobile: mobile?
+      },
+
+      user: {
+        id: current_user&.id,
+        email: current_user&.email,
+        country: current_user&.country,
       }
     }
   end
