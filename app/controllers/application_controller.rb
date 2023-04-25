f25e30b11d1f0441ce04aa7966a8da2c8a381b1c
Jeffrey Chupp <jeff@semanticart.com>
2023-04-25 14:03:00 -0400

    Targeted log levels and context

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  around_action do |_, block|
+    $prefab.with_context(prefab_context, &block)
+  end
+
+  def prefab_context
+    {
+      device: {
+        mobile: mobile?
+      }
+    }
+  end
+
+  def mobile?
+    request.user_agent&.match?(/(iPhone|iPod|iPad|Android)/)
+  end
 end
